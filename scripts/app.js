724aae52add7f6d6c389cc4ae60e7cbc5bac7c6a
Ciro Ferreira da Cruz <ciro.cfc@gmail.com>
2016-01-27 11:12:24 -0200

    changed to requirejs

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
-define('app', ['routes', 'aboutController', 'contactController'], function(routes, aboutController, contactController){
+define('app', ['angular', 'uiRouter', 'routes', 'aboutController', 'contactController'],
+	function(angular, uiRouter, routes, aboutController, contactController){
 	'use strict';
 
-	var app = angular.module('app', ['ui.router.state']);
+	var app = angular.module('app', ['ui.router']);
 	app.config(routes);
 	app.controller('aboutController', aboutController);
 	app.controller('contactController', contactController);
--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,25 @@
 
 	require.config({
 		paths: {
+			'angular': '/bower_components/angular/angular.min',
+			'uiRouter': '/bower_components/angular-ui-router/release/angular-ui-router.min',
 			'app': '/scripts/app',
 			'routes': '/scripts/routes',
 			'aboutController': '/scripts/aboutController',
 			'contactController': '/scripts/contactController'
-		}
+		},
+		shim: {
+			'angular': {
+				exports : 'angular'
+			},
+			'uiRouter': {
+				deps: ['angular']
+			}
+		},
+		deps: ['app']
 	});
 
-	require(['app'], function(){
+	require(['angular', 'app'], function(angular, app){
 			angular.bootstrap(document, ['app']);
 		}
 	);
